3f291e027644906f220e2ad544145846aea3b61d
YashMasane <masaneyash6@gmail.com>
2025-03-07 15:09:50 +0530

    new portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DataSciencePortfolio from './components/DataSciencePortfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <DataSciencePortfolio />
+    </div>
+  );
+}
+
+export default App;
